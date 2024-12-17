413f6fa36d4e269e29062c19101bd4465aa57e11
Aly <aly.magassouba@nottingham.ac.uk>
2024-12-17 17:50:43 +0000

    Update test_bathing.py with reaching a specified grasping point

--- test_bathing.py
+++ test_bathing.py
@@ -1,9 +1,12 @@
 import json
 import utils
 from pyrcareworld.envs.bathing_env import BathingEnv
+import bathing_perception as perception
 import numpy as np
+import math
 import cv2
 import argparse
+
 
 
 def _main(use_graphics=False, dev=None):
@@ -25,17 +28,66 @@ def _main(use_graphics=False, dev=None):
 
         print(text) 
     # Initialize the environment
-    random_seed= utils.manikin_randomizer(True)
+    random_seed = utils.manikin_randomizer(True) # Comment for submission
     print(random_seed)
     env = BathingEnv(graphics=use_graphics, seed=random_seed) if dev == False else BathingEnv(graphics=use_graphics, executable_file="@editor")
     robot = env.get_robot()
-    robot = utils.random_robot_pose(robot)
-    env.step()
     sponge = env.get_sponge()
-    print(sponge.data)
+
+    # read waypoints from json file
+    with open('waypoints.json') as f:
+        waypoints_data = json.load(f)
+        print(waypoints_data)
+    
+    print(robot.data['position'])
+    print(robot.data['rotation'])
+
+    # random robot pose and sponge. Comment this section for submission! 
+    robot = utils.random_robot_pose(robot)
     sponge = utils.random_sponge_pose(sponge)
-    env.step()
-    print(sponge.data)
+    env.step(100) #waiting for robot and sponge to stabilise
+    #----------------------------------------------#
+
+
+    # loading grasping position for sponge
+    grasping_position = waypoints_data['grasping']['position']
+    grasping_rotation = waypoints_data['grasping']['rotation']
+    #env.Pend() to record videos
+
+    # calculating orientation and distance
+    direction, rot, dist = utils.move(robot.data['position'], grasping_position, robot.data['rotation'][1])
+
+    print("direction: ", direction)
+    print("rot: ", rot)
+    print("dist: ", dist)
+
+    if direction == "Left" :
+        robot.TurnLeft(rot, 1)
+    else:
+        robot.TurnRight (rot, 1)
+
+    # performing rotation action with required time step
+    env.step(utils.calculate_step_rotation(rot)) 
+
+    # moving forward
+    robot.MoveForward(dist, 1)
+    env.step(utils.calculate_step_translation(dist))
+
+    # performing rotation action with required time step
+    direction, rot  = utils.rotate(robot.data['rotation'][1], grasping_rotation)
+    if direction == "Left" :
+        robot.TurnLeft(rot, 1)
+    else:
+        robot.TurnRight (rot, 1)
+    env.step(utils.calculate_step_rotation(rot))
+
+
+    print(robot.data['position'])
+    print(robot.data['rotation'])
+
+    env.Pend()
+
+
 
 
 if __name__ == "__main__":
